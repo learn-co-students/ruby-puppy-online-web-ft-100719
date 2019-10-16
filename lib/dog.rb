badc2da91d841550c9546eaf7a190395097d1ee7
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-16 21:25:03 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
 # Add your code here
+class Dog
+attr_reader :name  
+@@all=[]
+
+  def initialize(name)
+    @name=name
+    # @@all<< self
+    save
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all=[]
+  end
+  
+  def self.print_all
+    @@all.each {|dog| puts "#{dog.name}"}
+  end
+  
+  def save
+    @@all<< self
+  end
+  
+end
