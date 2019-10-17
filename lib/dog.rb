56660fc26de45c684e7a2c3b68a3ea701f264c6e
Cristal Alatorre Perez <cristalcodes@gmail.com>
2019-10-17 12:35:44 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+class Dog
+
+  @@all=[]
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name=name
+    # @@all<<self
+    save
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    puts @@all.to_s.split('"').delete_if{|item| item.include?("<")||item.include?(">")}
+  end
+
+  def save
+    @@all<<self
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+end
