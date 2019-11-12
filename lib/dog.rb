b424f2f16f1e8728eb397a2f0750a8f89b94179b
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-11-12 20:07:37 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Add your code here
+class Dog
+  @@all=[]
+  def initialize (name)
+    @name=name
+    @@all<<self
+  end
+  def self.all
+    @@all
+  end
+  def self.print_all
+    @@all.each do |x|
+      puts "#{name}"
+    end
+  end
+end# Add your code here
